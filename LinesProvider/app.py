d80d76c1b5c9c499d18e8c06677e05e1653d5b26
Adrian Sadłocha <adrian.adek@gmail.com>
2017-06-02 03:05:55 +0200

    app.py: stylistic changes

--- LinesProvider/app.py
+++ LinesProvider/app.py
@@ -5,9 +5,12 @@ with open('../../processing-tools/out.json') as f:
     whole_json = json.loads(''.join(f.readlines()))
 
 app = Flask(__name__)
+
+
 @app.route("/api/lines")
 def lines():
     return jsonify(list(whole_json.keys()))
+
 
 @app.route("/api/route/<line_num>")
 def rotes(line_num):
