ee1ed2760c65a9c54897cfa431a09d653ff22c98
rafal <rafal.buzun@gmail.com>
2017-06-02 01:32:12 +0000

    fixed CORS error

--- LinesProvider/app.py
+++ LinesProvider/app.py
@@ -1,18 +1,25 @@
 import json
 from flask import Flask, jsonify
+from flask.ext.cors import CORS, cross_origin
+
+
 
 with open('../../processing-tools/out.json') as f:
     whole_json = json.loads(''.join(f.readlines()))
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/api/lines")
+@cross_origin()
 def lines():
-    return jsonify(list(whole_json.keys()))
+    return jsonify({'lines': list(whole_json.keys())})
 
 
 @app.route("/api/route/<line_num>")
+@cross_origin()
 def rotes(line_num):
     return jsonify(whole_json[str(line_num)])
 
